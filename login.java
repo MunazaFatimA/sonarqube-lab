6088eced7a0f43196ea4470d08f1242a6d389e95
MunazaFatimA <fatimamunaza300@gmail.com>
2026-05-03 16:14:52 +0500

    Add Login class with simple authentication
    Implement basic login functionality with hardcoded password check.

---
+++ login.java
@@ -0,0 +1,8 @@
+public class Login {
+    public static void main(String[] args) {
+        String password = "admin123"; // Hardcoded password
+        if(password.equals("admin123")) {
+            System.out.println("Login Successful");
+        }
+    }
+}
